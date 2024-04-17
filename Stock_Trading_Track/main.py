8d9b2ade194b4dbef9862850400e7ee2d89a3ca9
HarshanaPrabhath <harshanaprabhath147@gmail.com>
2024-04-17 12:38:02 +0530

    completed task - sending massage

--- Stock_Trading_Track/main.py
+++ Stock_Trading_Track/main.py
@@ -1,4 +1,5 @@
 import requests
+from twilio.rest import Client
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -6,9 +7,15 @@ COMPANY_NAME = "Tesla Inc"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
+# twilio sms alert api setup
+
+account_sid = 'AC7ea75f8aa0a1391623fe473265e30aa2'
+auth_token = '2e7182f69a450cc2d38842d7345e73d9'
+client = Client(account_sid, auth_token)
+
 # stock_api_call
 
-STOCK_API_KEY = "UOPCN0EKYN9WQD01"
+STOCK_API_KEY = "9F5505RYULH3MRQJ"
 
 stock_params = {
     "function": "TIME_SERIES_DAILY",
@@ -48,28 +55,23 @@ news_api_parameters = {
 
 response_news_api = requests.get(NEWS_ENDPOINT, news_api_parameters)
 response_news_api = response_news_api.json()
-print(response_news_api["articles"][0:3]["title"])
-print(response_news_api["articles"][1]["title"])
-print(response_news_api["articles"][2]["title"])
 
-# TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+# articles headlines
+articles = response_news_api["articles"][0:3]
+
+# articles_title_2 = response_news_api["articles"][1]["title"]
+# articles_title_3 = response_news_api["articles"][2]["title"]
+
+massage = articles
+
+# twilio msg (articles headlines) send to relevant phone number
+for i in range(3):
+    message = client.messages.create(
+        from_='+12513062558',
+        body=response_news_api["articles"][i]["title"],
+        to='+94763384586'
+    )
+
+    print(message.sid)
 
 
-## STEP 3: Use twilio.com/docs/sms/quickstart/python
-# to send a separate message with each article's title and description to your phone number.
-
-# TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-# TODO 9. - Send each article as a separate message via Twilio.
-
-
-# Optional TODO: Format the message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
